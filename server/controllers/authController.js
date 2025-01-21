9e84b65f57a02d1b873838513b4a8dd34fe02103
Haiul <haiulsk037@gmail.com>
2025-01-21 18:39:57 +0530

    AuthController

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,46 +1,77 @@
-import bcrypt from 'bcryptjs';
-import jwt from 'jsonwebtoken'
-import userModel from '../model/userModels';
-export const register = async (req,res) => {
+const {validationResult} = require('express-validator');
+const blackListTokenModel= require('../model/blacklistToken.model.js');
+const userModel = require('../model/userModels.js');
+const userService = require('../services/user.service.js');
+const { model } = require('mongoose');
 
-    const {name,email,password} = req.body
 
-    if(!name || !email || !password){
-        return res.json({succes:false,message:'Missing Details'})
+module.exports.registerUser = async (req,res,next) => {
+
+    const errors = validationResult(req)
+    if(!errors.isEmpty()) {
+        return res.status(400).json({errors : errors.array()})
     }
+    console.log(req.body) 
 
-    try{
+    const {name,email,password} = req.body;
 
-        const existingUser = await userModel.findOne({email})
+    const isUserAlreadyExits = await userModel.findOne({email});
 
-        if(existingUser){
-            return res.json({succes:false,message:'User Existing'})
-        }
-
-        const hashPassword = await bcrypt.hash(password,10)
-        const user =new userModel({name,email,password:hashPassword});
-
-        await user.save();
-
-        const token = jwt.sign({id:user._id},process.env.JWT_SECRET,{expiresIn:'7d'})
-
-        res.cookie('token',token {
-            httpOnly:true,
-            secure:process.env.NODE_ENV === 'production',
-            sameSite:process.env.NODE_ENV === 'production' ? 
-            'none':'Strick',
-            maxAge: 7  * 24 * 160 * 60 * 1000
-        })
-
-    }catch(error){
-        res.json({succes:false,message:error.message})
+    if(isUserAlreadyExits){
+        return res.status(400).json({message:"User already exits"});
     }
+    
+    const hashPassword = await userModel.hashPassword(password);
+
+    const user = await userService.createUser({
+        name,
+        email,
+        password:hashPassword
+    });
+
+    const token = user.generateAuthToken()
+
+    res.status(201).json({message:'Register Succesfull',token,user});
 }
 
-export const login = async (req,res) => {
-    const {email,password} = req.body;
+module.exports.loginUser = async (req, res, next) => {
 
-    if(!email || !password){
-        return res.json({})
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
     }
+
+    const { email, password } = req.body;
+
+    const user = await userModel.findOne({email}).select('+password');
+
+    if (!user) {
+        return res.status(401).json({ message: 'Invalid email or password' });
+    }
+
+    const isMatch = await user.comparePassword(password);
+
+    if (!isMatch) {
+        return res.status(401).json({ message: 'Invalid email or password' });
+    }
+
+    const token = user.generateAuthToken();
+
+    res.cookie('token', token);
+
+    res.status(200).json({message:'Login Succesfull', token, user });
+} 
+
+module.exports.getUserProfile = async (req,res,next) => {
+    res.status(200).json({ message: "User Retrive",user:req.user});
+}
+
+module.exports.logOutUser = async (req,res,next) => {
+    res.clearCookie('token');
+
+    const token = req.cookies.token || req.headers.authorization.split(' ')[1];
+
+    await blackListTokenModel.create({token});
+
+    res.status(200).json({message:"Logout Succesfull"});
 }
